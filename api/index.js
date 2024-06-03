875faf64aa05c5cab1947a48f72fcb9efaf5e3ce
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2024-06-03 20:28:20 +0600

    fix uri

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 mongoose.set("strictQuery", true);
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongoDB!");
   } catch (error) {
     console.log(error);
