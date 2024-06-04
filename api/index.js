9462da3fc42f3df8a8d98c17233c2f14a00684a2
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2024-06-05 00:48:54 +0600

    push auth

--- api/index.js
+++ api/index.js
@@ -47,10 +47,4 @@ app.get("/", (req, res) => {
   res.status(201).json({ message: "Connected to Server!" });
 });
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 module.exports = app;
