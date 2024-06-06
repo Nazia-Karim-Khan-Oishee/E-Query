4877ad77fa3140f6704371d647a588617044fe38
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2024-06-06 22:54:04 +0600

    change in route protection

--- client/src/protectedRoutes.jsx
+++ client/src/protectedRoutes.jsx
@@ -1,15 +1,7 @@
-import { useEffect } from "react";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 
 const ProtectedRoute = (props) => {
   const user = JSON.parse(localStorage.getItem("user"));
-  const navigate = useNavigate();
-
-  //   useEffect(() => {
-  //     if (user) {
-  //       navigate("/");
-  //     }
-  //   }, [user, navigate]);
 
   if (user) {
     return <Outlet {...props} />;
