4ccdfcea83b647c53e14c6b39055e87ff0c36ee8
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2024-06-06 22:53:24 +0600

    modification

--- client/src/protectedRoutes.jsx
+++ client/src/protectedRoutes.jsx
@@ -5,11 +5,11 @@ const ProtectedRoute = (props) => {
   const user = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (user) {
-      navigate("/");
-    }
-  }, [user, navigate]);
+  //   useEffect(() => {
+  //     if (user) {
+  //       navigate("/");
+  //     }
+  //   }, [user, navigate]);
 
   if (user) {
     return <Outlet {...props} />;
